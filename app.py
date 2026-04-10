915401b6e88c09ac2340a75c3892fbd2f33d5062
chandrikakommoju-01 <chamdrikadevikommoju@gmail.com>
2026-04-10 11:44:21 +0530

    fix tables for gunicorn

--- app.py
+++ app.py
@@ -804,6 +804,7 @@ def create_tables():
 # ─────────────────────────────────────────────
 # RUN
 # ─────────────────────────────────────────────
+create_tables()
 if __name__ == '__main__':
     create_tables()
     app.run(debug=True, port=5000)
